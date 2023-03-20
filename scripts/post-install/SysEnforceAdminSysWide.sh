f7bf9dc88630f03b3264a47677d17d03b2faeacc
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-03-20 11:34:42 -0700

    Update SysEnforceAdminSysWide.sh
    Format fixing more

--- scripts/post-install/SysEnforceAdminSysWide.sh
+++ scripts/post-install/SysEnforceAdminSysWide.sh
@@ -3,6 +3,5 @@ security authorizationdb read system.preferences > /tmp/system.preferences.plist
 YES (0)
 $ sudo defaults write /tmp/system.preferences.plist shared -bool false
 
-$ sudo security authorizationdb write system.preferences <
-/tmp/system.preferences.plist
+$ sudo security authorizationdb write system.preferences < /tmp/system.preferences.plist
 YES (0)
