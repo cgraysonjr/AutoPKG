bcfab6b26c0f3d7676641d5477851eed62369fc0
cgraysonjr <carmichael.grayson@csueastbay.edu>
2023-03-20 14:47:21 -0700

    Update SysEnforceAdminSysWide.sh
    removed sudo commands. Cleanup

--- scripts/post-install/SysEnforceAdminSysWide.sh
+++ scripts/post-install/SysEnforceAdminSysWide.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-security authorizationdb read system.preferences > /tmp/system.preferences.plist
-YES (0)
-$ sudo defaults write /tmp/system.preferences.plist shared -bool false
+/usr/bin/security authorizationdb read system.preferences > /tmp/system.preferences.plist
 
-$ sudo security authorizationdb write system.preferences <
-/tmp/system.preferences.plist
+YES (0)
+
+/usr/bin/defaults write /tmp/system.preferences.plist shared -bool false
+
+/usr/bin/security authorizationdb write system.preferences < /tmp/system.preferences.plist
+
 YES (0)
