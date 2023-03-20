7398d27629125bd01d982f1b9af35103a891a8d4
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-03-20 15:04:16 -0700

    script cleanup
    Removed expected responses from commands, not useful in scripts.

--- scripts/post-install/SysEnforceAdminSysWide.sh
+++ scripts/post-install/SysEnforceAdminSysWide.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 /usr/bin/security authorizationdb read system.preferences > /tmp/system.preferences.plist
 
-YES (0)
-
 /usr/bin/defaults write /tmp/system.preferences.plist shared -bool false
 
 /usr/bin/security authorizationdb write system.preferences < /tmp/system.preferences.plist
-
-YES (0)
