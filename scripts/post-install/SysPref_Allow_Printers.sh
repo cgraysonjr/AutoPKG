f40f54d17a8d1a4a78e73cf6d2b2a3a8d20cd2eb
cgraysonjr <carmichael.grayson@csueastbay.edu>
2023-03-22 14:41:55 -0700

    Update SysPref_Allow_Printers.sh
    Add comments

--- scripts/post-install/SysPref_Allow_Printers.sh
+++ scripts/post-install/SysPref_Allow_Printers.sh
@@ -1,12 +1,13 @@
 ﻿#!/bin/bash
 
-# Allows non-admin users to add printers.
+# Changes Pemissions to allow anyone to add and delete printers.
 
-#For printing
-
+#Modifies authorizationdb to allow the Everyone group access to the System Preferences Printer panel
 /usr/bin/security authorizationdb write system.preferences.printing allow
+#Modifies authorizationdb to allow the Everyone group access to the System Preferences Print Operator panel
 /usr/bin/security authorizationdb write system.print.operator allow
+# Adds the Everyone group to the lpadmin group
 /usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group lpadmin
+# Adds the Everyone group to the lpadmin group
 /usr/sbin/dseditgroup -o edit -n /Local/Default -a everyone -t group _lpadmin
-
 exit 0;
