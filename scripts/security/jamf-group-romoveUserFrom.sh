a4be31cd23c9916e208c66dfd32361670a173bd1
cgraysonjr <carmichael.grayson@csueastbay.edu>
2023-03-24 11:01:24 -0700

    Create jamf-group-romoveUserFrom.sh
    Added Script for testing

---
+++ scripts/security/jamf-group-romoveUserFrom.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+accountCheck=("itwsupport")
+
+node=${2:-"/Local/Default"}
+
+for u in $(dscl "$node" list /Users ); do
+	echo $u
+	#/usr/sbin/dseditgroup -o edit -d $u -t user admin
+fi
+
+done
