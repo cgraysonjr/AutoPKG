9af2719d2f60eabeeabd56f463c17ecd74bf3cd3
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-03-29 09:28:32 -0700

    Reformat script
    Reformat script

--- scripts/security/jamf-group-romoveUserFrom.sh
+++ scripts/security/jamf-group-romoveUserFrom.sh
@@ -1,4 +1,6 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 accountCheck=("root" $4 $5 $6)
+echo $accountCheck
 adminsLocal=$(dscacheutil -q group -a name admin | awk '/users: / {$1=""; print $0}')
+echo $adminsLocal
