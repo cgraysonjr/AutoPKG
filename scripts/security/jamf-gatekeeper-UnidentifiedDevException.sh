b341b8caf4e966b73afcb7aeea8faf28579a56b8
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-04-12 14:19:32 -0700

    Update jamf-gatekeeper-UnidentifiedDevException.sh
    script updated for ensures success and comments

--- scripts/security/jamf-gatekeeper-UnidentifiedDevException.sh
+++ scripts/security/jamf-gatekeeper-UnidentifiedDevException.sh
@@ -2,3 +2,7 @@
 # Removes the given application from the apple quarantine list <com.apple.quarantine>.
 # Prevents the computer from blocking the given app from running.
 xattr -d com.apple.quarantine "/Applications/$4"
+
+# Ensures success code of script run.
+# If attribute doesn't exist, above command railes to remove it.
+exitcode=0
