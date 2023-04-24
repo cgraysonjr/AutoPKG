39cce56092b1497fded5e2cc0634cf2614fd38da
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-04-24 11:31:59 -0700

    Update SysPref_Enforce_AdminSysWide.sh
    Description correction

--- scripts/post-install/SysPref_Enforce_AdminSysWide.sh
+++ scripts/post-install/SysPref_Enforce_AdminSysWide.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#Users will need to enter their password to unlock some additional preference panes that are unlocked by default like Network, Startup and Printers & Scanners.
 # CIS Benchmark
 #2.5.9 Ensure an Administrator Password Is Required to Access System-Wide Preferences (Automated)
+
+# Users will need to enter their password to unlock some additional preference panes that 
+# are unlocked by default like Network, Startup and Printers & Scanners.
 
 /usr/bin/security authorizationdb read system.preferences > /tmp/system.preferences.plist
 /usr/bin/defaults write /tmp/system.preferences.plist shared -bool false
