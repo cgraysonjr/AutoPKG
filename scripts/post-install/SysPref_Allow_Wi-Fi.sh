ad5cc9b063ff54d28986759a518e1eeb3a908b6a
cgraysonjr <carmichael.grayson@csueastbay.edu>
2023-04-28 09:38:43 -0700

    Items moved
    Items moved to developer folders

---
+++ scripts/post-install/SysPref_Allow_Wi-Fi.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+# Workaround as shown in https://www.jamf.com/jamf-nation/discussions/19050/add-wifi-networks-without-admin-privileges
+# Allows non-admin users to manage their WiFi configuration.
+
+#For WiFi
+
+/usr/bin/security authorizationdb write system.preferences.network allow
+/usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
+
+
+exit 0;
