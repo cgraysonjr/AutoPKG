7c26e4e74727683e74d31610fd76446359a62782
cgraysonjr <94399678+cgraysonjr@users.noreply.github.com>
2023-05-01 11:37:59 -0700

    Update service-installed-rosetta.sh
    bash else if fixed and comments added

--- scripts/jamf-extensions/service-installed-rosetta.sh
+++ scripts/jamf-extensions/service-installed-rosetta.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
+# Check if the devices is a M-series processor
 if [[ !($(uname -m) == 'arm64') ]]; then
-  result="Not Applicable"
-else if [[/usr/bin/pgrep oahd >/dev/null 2>&1]]; then
-	result= "Installed"
+	# If device isn't M series, set results to "Not Applicable"
+	result="Not Applicable"
+elif [[/usr/bin/pgrep oahd >/dev/null 2>&1]]; then
+	result="Installed"
 else
 	result="Not Installed"
 fi
