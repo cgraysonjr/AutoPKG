f644dec9285df9e3bcc025f52bca6e4e552ba7de
cgraysonjr <carmichael.grayson@csueastbay.edu>
2024-05-08 10:46:15 -0700

    Create google.chrome.userLibrary.remove.sh
    Upload

---
+++ scripts/self-help/google.chrome.userLibrary.remove.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+#############################################
+# find the logged in user 					#
+#############################################
+
+currentUser=$(who | awk '/console/{print $1}')
+echo $currentUser
+
+# Kill Chrome if running
+killall "Google Chrome"
+
+# Remove Chrome User Profile
+rm -r /Users/"$currentUser"/Library/Application\ Support/Google/Chrome/
