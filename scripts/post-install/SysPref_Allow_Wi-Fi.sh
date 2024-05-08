10c4522a3fad2d6d90db0acbf0c1e56f36b5d9c3
cgraysonjr <carmichael.grayson@csueastbay.edu>
2024-05-08 10:33:42 -0700

    Update SysPref_Allow_Wi-Fi.sh
    New line necessary for Wi-Fi modification.
    SO 14.0 Ventura Issues:  Still request Admin, but doesn't require admin credential to complete.

--- scripts/post-install/SysPref_Allow_Wi-Fi.sh
+++ scripts/post-install/SysPref_Allow_Wi-Fi.sh
@@ -8,5 +8,5 @@
 /usr/bin/security authorizationdb write system.preferences.network allow
 /usr/bin/security authorizationdb write system.services.systemconfiguration.network allow
 
-
+/usr/bin/security authorizationdb write com.apple.wifi allow
 exit 0;
